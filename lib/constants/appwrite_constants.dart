d93ade992ec7878a7b1d1bc94ae5722af7cb401f
muhamad iksan <muhamad.535210077@stu.untar.ac.id>
2023-04-11 14:43:22 +0700

    committed

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '64342c170ccbcb45fc5b';
+  static const String projectId = '6433d66b582f2106c140';
+  static const String endPoint = 'http://localhost:80/v1';
+}
